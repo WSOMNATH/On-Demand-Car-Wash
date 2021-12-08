d575e8df948042416377eb3ea8c202678b1e2a2d
wsomnath <wsomnath@LIN25007809.corp.capgemini.com>
2021-12-08 22:33:14 +0530

    secound commit

---
+++ Order/src/main/java/com/ondemandcarwash/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.ondemandcarwash.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/order")
+public class TestController {
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, from Order";
+		
+	}
+
+}
